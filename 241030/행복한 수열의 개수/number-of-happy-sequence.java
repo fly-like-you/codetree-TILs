c0d5e96bf80c455d93a230f1d6964042360e6069
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-10-30 22:18:55 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241030/행복한 수열의 개수/number-of-happy-sequence.java
+++ 241030/행복한 수열의 개수/number-of-happy-sequence.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+package src.algo1030;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,7 +16,7 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        
+
         arr = new int[N][N];
         seq = new int[N];
 
@@ -22,35 +26,43 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
+
+
         int answer = 0;
+        // row search
         for (int i = 0; i < N; i++) {
+            // seq에 행 값 입력해주기
             for (int j = 0; j < N; j++) seq[j] = arr[i][j];
             if (isHappySequence()) answer++;
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) seq[j] = arr[j][i];
+        // column search
+        for (int j = 0; j < N; j++) {
+            // seq에 열 값 입력해주기
+            for (int i = 0; i < N; i++) seq[i] = arr[i][j];
+
             if (isHappySequence()) answer++;
         }
+
         System.out.println(answer);
     }
+
     private static boolean isHappySequence() {
-        // 1 2 2 2 3 4 5
+        // seq가 연속한지 확인하려면
+        // 1. 같지 않은 경우 l길이를 초기화
+        // 2. 같은 경우 l 길이를 늘려준다
         int l = 0;
         int cur = seq[0];
-        int maxCnt = 0;
+        int longest = 0;
         for (int i = 0; i < N; i++) {
-            if (cur != seq[i]) {
+            if (cur == seq[i]) l++;
+            else {
                 l = 1;
                 cur = seq[i];
-            } else {
-                l++;
             }
 
-            maxCnt = Math.max(maxCnt, l);
-            
+            // 항상 최대로 길었던 값을 계속 비교
+            longest = Math.max(longest, l);
         }
-        
-        return maxCnt >= M;
+        return longest >= M;
     }
 }
