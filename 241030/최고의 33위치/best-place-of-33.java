720e8b9b2ad8f146196b1f56b8072bcbd99fbfe7
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-10-30 20:38:31 +0900

    Title: 최고의 33위치, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241030/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[][] arr;
+    static int range = 3;
+    static int N;
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = 0; j < N - 2; j++) {
+                answer = Math.max(answer, find(i, j));
+            }
+        }
+        
+        System.out.println(answer);
+    }
+
+    private static int find(int x, int y) {
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        return sum;
+    }
+}
