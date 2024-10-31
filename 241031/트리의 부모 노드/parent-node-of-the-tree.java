4ae9fb9487d5225de778115e95adfe63259bbffc
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-10-31 23:19:33 +0900

    Title: 트리의 부모 노드, Time: 1666ms, Memory: 49MB, Status: Passed - CodeTree

--- 241031/트리의 부모 노드/parent-node-of-the-tree.java
+++ 241031/트리의 부모 노드/parent-node-of-the-tree.java
@@ -2,46 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static List[] adj;
+    static List<Integer>[] adj;
     static int N;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-
+        
         adj = new List[N + 1];
-        for (int i = 0; i < N + 1; i++) {
-            adj[i] = new ArrayList<Integer>();
+        for (int i = 1; i <= N; i++) {  // 1부터 시작하여 불필요한 반복 제거
+            adj[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < N - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            String[] tokens = br.readLine().split(" ");
+            int from = Integer.parseInt(tokens[0]);
+            int to = Integer.parseInt(tokens[1]);
             adj[from].add(to);
             adj[to].add(from);
         }
 
         int[] parents = new int[N + 1];
         dfs(parents, 1);
-        for (int i = 2; i < N + 1; i++) {
+
+        for (int i = 2; i <= N; i++) {
             System.out.println(parents[i]);
         }
     }
 
-    private static void dfs(int[] visited, int cur) {
-
-        for (int i = 0; i < adj[cur].size(); i++) {
-            int node = (int) adj[cur].get(i);
-            if (visited[node] != 0) continue;
-            visited[node] = cur;
-            dfs(visited, node);
+    private static void dfs(int[] parents, int cur) {
+        for (int node : adj[cur]) {
+            if (parents[node] != 0) continue;
+            parents[node] = cur;
+            dfs(parents, node);
         }
     }
 }
