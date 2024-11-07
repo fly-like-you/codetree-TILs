9dd94a0729b42a037d1e86ba2df917af804b0837
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-11-07 21:19:22 +0900

    Title: 삼각형 컨베이어 벨트, Time: 94ms, Memory: 8MB, Status: Runtime Error - CodeTree

--- 241107/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
+++ 241107/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -1,34 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int[] container;
+    static int[][] container;
     static int N, T;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
-        container = new int[N * 3];
+        container = new int[3][N];
 
-        for (int i = 0; i < 3 * N; i++) {
-            if (st == null || !st.hasMoreTokens()) {
-                st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                container[i][j] = Integer.parseInt(st.nextToken());
             }
-            container[i] = Integer.parseInt(st.nextToken());
         }
+
         T = T % (N * 3);
-        for (int t = 0; t < T; t++) {
-            int temp = container[3 * N - 1];
-            for (int i = 3 * N - 1; i > 0; i--) {
-                container[i] = container[i - 1];
+        while (T-- > 0) {
+            int temp1 = container[0][N-1];
+            for (int i = N - 1; i > 0; i--) {
+                container[0][i] = container[0][i-1];
             }
-            container[0] = temp;
+            container[0][0] = temp1;
+
+            int temp2 = container[1][N-1];
+            for (int i = N - 1; i > 0; i--) {
+                container[1][i] = container[1][i-1];
+            }
+            container[1][0] = temp2;
+
+            int temp3 = container[2][N-1];
+            for (int i = N - 1; i > 0; i--) {
+                container[2][i] = container[2][i-1];
+            }
+            container[2][0] = temp3;
         }
 
         StringBuilder sb = new StringBuilder();
